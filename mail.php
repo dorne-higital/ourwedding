7059c7842515b69bc47434e7f603509c53ff13bc
Dan <dan.horne@nexuspoint.co.uk>
2023-11-27 18:56:28 +0000

    update response

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@ try {
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo '<script> window.location="../index.html"; </script>';
+    echo '<script> window.location="../response.html"; </script>';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
